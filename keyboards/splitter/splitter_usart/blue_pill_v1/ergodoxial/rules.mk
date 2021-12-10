779b53efd812bc0b25262030a27b633f68fb41c2
semicontinuity <igor.a.karpov@gmail.com>
2021-12-10 17:14:31 +0300

    cleanup

--- keyboards/splitter/splitter_usart/blue_pill_v1/ergodoxial/rules.mk
+++ keyboards/splitter/splitter_usart/blue_pill_v1/ergodoxial/rules.mk
@@ -20,12 +20,6 @@ AUDIO_ENABLE = no           # Audio output
 #LAYOUTS = ergodox_pretty_pedals
 LAYOUTS = ergodox
 
-# MCU name
-MCU = STM32F103
-
-# Bootloader selection
-BOOTLOADER = stm32duino
-
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
