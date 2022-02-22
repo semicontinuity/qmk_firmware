546046b5c109fe2d245b1c9060cf2e27d96a61f3
semicontinuity <igor.a.karpov@gmail.com>
2022-02-22 10:03:52 +0300

    just-split: experimenting with housekeeping_task_user. one-shot not working.

--- keyboards/splitter/splitter_usart/black_pill_v1/just_split/keymaps/default/raw_hid_receive_impl.c
+++ keyboards/splitter/splitter_usart/black_pill_v1/just_split/keymaps/default/raw_hid_receive_impl.c
@@ -3,13 +3,14 @@
 #include "splitter_usart.h"
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
-    set_lock(LOCK_RUS, data[0] & LOCK_RUS);
-    indicate();
+//    set_lock(LOCK_RUS, data[0] & LOCK_RUS);
+//    indicate();
 }
 
 
 bool led_update_user(led_t led_state) {
-    set_lock(LOCK_SHIFT, led_state.caps_lock);
-    indicate();
+//    set_lock(LOCK_SHIFT, led_state.caps_lock);
+//    indicate();
+    layer_or(layer_state); // just trigger layer_state_set_user
     return true;
 }
--- keyboards/splitter/splitter_usart/black_pill_v1/just_split/rules.mk
+++ keyboards/splitter/splitter_usart/black_pill_v1/just_split/rules.mk
@@ -10,15 +10,15 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 #CONSOLE_ENABLE = yes        # Console for debug
 CONSOLE_ENABLE = no
 
-COMMAND_ENABLE = yes         # Commands for debug and configuration
+#COMMAND_ENABLE = yes         # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
+
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-
-# LAYOUTS = split_3x5_3_P
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE -DHALVES_HAVE_DISTINCT_SCAN_CODES=TRUE
